54d9ae4039e1961d7db4ae1452289cf7c39a93b9
devin <devin.caines@gmail.com>
2023-01-20 18:41:20 -0500

    pokedex project

--- pokemon.js
+++ pokemon.js
@@ -1,1 +1,6 @@
-console.log ('javascript sucks') ;
+// pokedex project!
+// work to be done. 
+// list of pokemon (maybe original 151 to keep it easy)
+// list by type, name in alphabetical order, google or scrape text file of pokemon 
+// search function
+// if we wanna be really spicy-- an application that lets u see a pic of the pokemon and a sound file for when they make noise
